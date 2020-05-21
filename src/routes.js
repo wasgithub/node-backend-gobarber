08179f34363e3bf670934fdbfb88e931bd96c77c
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2020-05-21 18:19:15 -0300

    LOADER_MODEL USERS - Criando loader de models.mp4

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/Users';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'washington Alexandre',
+    email: 'was.alexandre42@gmail.com',
+    password_hash: '12345',
+  });
+
+  return res.json(user);
 });
 
 export default routes;

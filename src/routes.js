fdb6773a66443f731864121612b61566bf477723
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2020-05-21 18:57:29 -0300

    CADASTRO USUARIO: POST - Cadastro de usuários

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/Users';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'washington Alexandre',
-    email: 'was.alexandre42@gmail.com',
-    password_hash: '12345',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;

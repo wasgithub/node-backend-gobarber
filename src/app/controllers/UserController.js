854e0b5131e2749b12d74bba1904ba57948bf637
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2020-05-21 20:33:26 -0300

    JWT - AUTH: Autenticação JWT

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -4,6 +4,7 @@ class UserController {
   async store(req, res) {
     const userExist = await User.findOne({ where: { email: req.body.email } });
 
+    // verifica se usuário já existe
     if (userExist) {
       return res.status(400).json({ error: 'User aready exists.' });
     }
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '3aaa5d2b3c0d663740169a011458c913',
+  expiredsIn: '7d',
+};

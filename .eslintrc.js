372e88227684aadff1bf420293f2418d004e721d
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2020-05-21 14:25:23 -0300

    ESLINT: Adicionado esliint e configuracao

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb-base'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
+};

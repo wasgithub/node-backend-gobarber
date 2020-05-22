c963d996dadb586512e5d417a099c4b1f0702070
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2020-05-21 21:59:27 -0300

    USER_UPDATE:  - Update do usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -19,8 +19,31 @@ class UserController {
     });
   }
 
-  update(req, res) {
-    return res.json({ ok: true });
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExist = await User.findOne({ where: { email } });
+
+      if (userExist) {
+        return res.status(400).json({ error: 'User aready exist' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match.' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
